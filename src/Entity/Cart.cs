c82ab5e606cc0fd2417723a09d861ec812b3c5bc
TalalMAlqaeni <talal.m.s.alqarni@gmail.com>
2024-09-19 18:22:00 +0300

    cart

---
+++ src/Entity/Cart.cs
@@ -0,0 +1,12 @@
+namespace sda_3_online_Backend_Teamwork.src.Entity
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public List<Product> Products { get; set; }
+        public int CartQuantity { get; set; }
+        public decimal TotalPrice { get; set; }
+
+    }
+}
