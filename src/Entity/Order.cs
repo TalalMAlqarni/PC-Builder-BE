2770e450559f0b0f56149dd6f5cd1b5f3070ec8d
AbdulazizMohammad <opomhmd@hotmail.com>
2024-09-23 15:19:58 +0300

    fix UpdateShipDate()

--- src/Entity/Order.cs
+++ src/Entity/Order.cs
@@ -8,14 +8,15 @@ namespace src.Entity
     public class Order
     {
         public Guid Id { get; set; }
-        public Guid PaymentId { get; set; }
         public Guid UserId { get; set; }
+        public Guid CartId { get; set; }
+        public Guid PaymentId { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime ShipDate { get; set; }
-        public string OrderStatus { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
         public int PostalCode { get; set; }
 
     }
