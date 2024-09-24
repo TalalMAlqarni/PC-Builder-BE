7502200c876b24636f07f0cbc8fc86a4ba174388
razanmtw17 <razan.m.tw@gmail.com>
2024-09-24 15:07:57 +0300

    add authorized and username attribute

--- src/Entity/User.cs
+++ src/Entity/User.cs
@@ -11,28 +11,14 @@ namespace src.Entity
     public class User
     {
         public Guid UserId { get; set; }
+        public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public DateOnly BirthDate { get; set; }
         public string Role { get; set; }
-        private string _password;
-        public string Password 
-        { 
-            get { return Encrypte(_password); }
-            set { _password = value; }
-        }
-        public static string Encrypte(string password)
-        {
-            using var sha = SHA256.Create();
-            byte[] bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
-            var sb = new StringBuilder();
-            for(int i = 0; i < bytes.Length; i++)
-            {
-                sb.Append(bytes[i].ToString("x2"));
-            }
-            return sb.ToString();
-        }
+        public string Password { get ; set ; }
+        public byte[]? Salt { get; set; }
     }
 }
