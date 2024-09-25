4832e78ac0ac9bb4f13b2a26ecc6374c9522e0e0
TalalMAlqaeni <talal.m.s.alqarni@gmail.com>
2024-09-25 16:55:31 +0300

    update .gitignore file

--- src/Database/DatabaseContext.cs
+++ src/Database/DatabaseContext.cs
@@ -7,8 +7,8 @@ namespace sda_3_online_Backend_Teamwork.src.Database
     public class DatabaseContext : DbContext
     {
 
-        public DbSet<Cart> Cart { get; set; }
-        public DbSet<CartDetails> CartDetails { get; set; }
+        // public DbSet<Cart> Cart { get; set; }
+        // public DbSet<CartDetails> CartDetails { get; set; }
         public DbSet<Category> Category { get; set; }
         public DbSet<Order> Order { get; set; }
         public DbSet<Payment> Payment { get; set; }
