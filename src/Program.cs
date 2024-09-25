b74c63c5a445b282f5402b2aa6c86c8abee39aaa
razanmtw17 <razan.m.tw@gmail.com>
2024-09-25 14:28:03 +0300

    connection string

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,12 @@
+using Npgsql;
+using Microsoft.EntityFrameworkCore;
+using src.Database;
 var builder = WebApplication.CreateBuilder(args);
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(builder.Configuration.GetConnectionString("Local"));
+builder.Services.AddDbContext<DatabaseContext>( options => {
+    options.UseNpgsql(dataSourceBuilder.Build());
+    }
+);
 //Add controllers
 builder.Services.AddControllers();
 // Add services to the container.
@@ -8,7 +16,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 
 
 app.MapControllers();
