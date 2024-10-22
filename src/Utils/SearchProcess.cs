d4d8f4f5d3fc1f83dfe5687866e8583ba57c3fcf
Jomana Mahjoob <qvthuht69@gmail.com>
2024-10-22 09:58:59 +0300

    Edited the filtering function , now it can filter based on many colors

--- src/Utils/SearchProcess.cs
+++ src/Utils/SearchProcess.cs
@@ -4,18 +4,19 @@ namespace src.Utils
 {
     public class SearchProcess
     {
-        /*first: user will search on a product name:  
+        /*first: user will search on a product name:
         NOTE : It'll also shows the result based on the products descreption
         */
-        public string Search { get; set; } =string.Empty;
+        public string Search { get; set; } = string.Empty;
 
         //second: user will filter the product based on specific criteria
 
-        
+
         public string? Name { get; set; }
-        public string? Color { get; set; }
-        
-        
+
+        // public string? Color { get; set; }
+        public List<string>? Colors { get; set; } = new List<string>();
+
         public decimal? MinPrice { get; set; }
         public decimal? MaxPrice { get; set; }
 
@@ -26,9 +27,8 @@ namespace src.Utils
 
         //fourth: user will implmenet pagination
 
-        public int Limit { get; set; }=5;
+        public int Limit { get; set; } = 5;
 
-        public int Offset { get; set; } =0;
-
+        public int Offset { get; set; } = 0;
     }
 }
